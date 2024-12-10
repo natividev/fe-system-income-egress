1067bba9a4aa0ef61ee77fea9bdf6c80e3f2d751
devjonatanmo <developerjonatanmorales@gmail.com>
2024-12-09 19:52:20 -0600

    CHORE: se agrego un redireccionamiento para el dashborad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
-
+import { redirect } from "next/navigation";
 export default function Home() {
-  return (
-    <Box>
-      <Text fontSize="6xl">hola mundo</Text>
-    </Box>
-  );
+  redirect("/dashboard");
 }
