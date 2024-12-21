2b60c331ca7ca0bd103060158639f5f32463696d
devjonatanmo <developerjonatanmorales@gmail.com>
2024-12-20 22:27:35 -0600

    FIT: tipos de proyectos

--- src/components/ui/selectControl/SelectControl.tsx
+++ src/components/ui/selectControl/SelectControl.tsx
@@ -57,8 +57,9 @@ export const SelectController = <T extends FieldValues>({
         }
         return element.label === dataFilters || element.value === dataFilters;
       });
+      console.log(dataFilter);
 
-      if (dataFilter) setValue(name, dataFilter[0]);
+      if (dataFilter) setValue(name, dataFilter);
     } catch (error) {
       setIsLoading(false);
       console.log(error);
